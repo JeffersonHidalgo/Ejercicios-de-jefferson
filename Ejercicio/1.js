348d049441d417a101c138d86c36454ed6117b0e
Jefferson <jeffersonhidalgo853@gmai.com>
2020-04-23 18:15:14 -0400

    Inicio de mi proyecto

---
+++ Ejercicio/1.js
@@ -0,0 +1,18 @@
+let resto;
+let numero1=prompt("ingrese primer valor");
+let numero2=prompt("ingrese segundo valor");
+
+resto= numero1%numero2;
+function Jefferson(resto){
+
+if(resto==0){
+    alert(numero1+"_es multiplo de_"+ numero2)
+}
+
+else{
+    alert(numero1+"_no es multiplo de_"+numero2)
+}
+}
+alert (Jefferson(resto))
+Ç
+
